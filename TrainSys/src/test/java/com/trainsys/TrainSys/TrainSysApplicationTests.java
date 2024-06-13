82b8c0b2b72d226974869bf4cdd62f929162fcb7
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-13 19:02:44 -0300

    adiciona estrutura spring inicial

---
+++ TrainSys/src/test/java/com/trainsys/TrainSys/TrainSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainsys.TrainSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
