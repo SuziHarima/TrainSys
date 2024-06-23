39a290fa1cd7d9c7ac5542b109f1676f6ef51649
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-22 22:44:30 -0300

    adiciona endpoint listar por estudante

--- TrainSys/src/main/java/com/trainsys/TrainSys/controller/WorkoutsController.java
+++ TrainSys/src/main/java/com/trainsys/TrainSys/controller/WorkoutsController.java
@@ -2,12 +2,15 @@ package com.trainsys.TrainSys.controller;
 
 
 import com.trainsys.TrainSys.controller.request.NewWorkoutRequest;
+import com.trainsys.TrainSys.controller.response.SearchWorkoutResponse;
 import com.trainsys.TrainSys.controller.response.WorkoutResponse;
 import com.trainsys.TrainSys.service.WorkoutService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("workouts")
@@ -21,5 +24,11 @@ public class WorkoutsController {
     public ResponseEntity<WorkoutResponse> saveWorkout(@RequestHeader(name = "Authorization") String token, @RequestBody NewWorkoutRequest newWorkoutRequest){
         log.info("POST /workout -> 201 CREATED");
         return ResponseEntity.ok(workoutService.workoutsRegistration(newWorkoutRequest,token.substring(7)));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SearchWorkoutResponse> listAllByStudentId(@RequestHeader(name = "Authorization") String token, @PathVariable Integer id){
+        log.info("GET /workout -> OK");
+        return ResponseEntity.ok(workoutService.searchAllByStudentId(token.substring(7), id));
     }
 }
--- TrainSys/src/main/java/com/trainsys/TrainSys/repository/WorkoutsRepository.java
+++ TrainSys/src/main/java/com/trainsys/TrainSys/repository/WorkoutsRepository.java
@@ -3,8 +3,12 @@ package com.trainsys.TrainSys.repository;
 
 import com.trainsys.TrainSys.entity.WorkoutsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WorkoutsRepository extends JpaRepository<WorkoutsEntity, Integer> {
+    List<WorkoutsEntity> findAllByStudentId(@Param("id") Integer id);
 }
